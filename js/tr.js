d1689b686c67ab70196c2cb847b14e5edb1b0086
plehegar <plh@w3.org>
2019-10-11 09:59:17 -0400

    mode no-cors

--- js/tr.js
+++ js/tr.js
@@ -2,12 +2,16 @@
 	 let API_KEY = "sdqupwhg51c0o4ww488w40og4kkog8s";
 
    function findLatest(shortname) {
-		return fetch("https://api.w3.org/specifications/" + shortname + "/versions/latest?apikey=" + API_KEY).then(res => res.json())
+		return fetch("https://api.w3.org/specifications/" + shortname + "/versions/latest?apikey=" + API_KEY, {
+			'mode': 'no-cors'
+		}).then(res => res.json())
 			.then(spec => {
         // extract the proper shortname
         spec.shortname = spec.shortlink.match("https?://www.w3.org/TR/([^/]+)/");
 
-				return fetch(spec._links.deliverers.href+ "?apikey=" + API_KEY).then(res => res.json()).then(deliverers => {
+				return fetch(spec._links.deliverers.href+ "?apikey=" + API_KEY, {
+					'mode': 'no-cors'
+				}).then(res => res.json()).then(deliverers => {
 					spec._deliverers = deliverers._links.deliverers;
 					return spec;
 				});
@@ -18,7 +22,9 @@
 				function fetchGroup() {
 					// avoid looking at submissions
 					if ((index < length) && (spec._deliverers[index].title !== "UNKNOWN WORKING GROUP")) {
-  				  return fetch(spec._deliverers[index].href+ "?apikey=" + API_KEY).then(res => res.json()).then(deliverer => {
+					  return fetch(spec._deliverers[index].href+ "?apikey=" + API_KEY, {
+							'mode': 'no-cors'
+						}).then(res => res.json()).then(deliverer => {
 						  spec.groups[index++] = deliverer;
 					    return fetchGroup();
 				    });
